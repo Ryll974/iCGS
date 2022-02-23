769a6fb65673196b20b079866b79b3237659b421
Ryll974 <jullien.patrick@gmail.com>
2022-02-23 14:34:08 +0400

    ver.0.18 -- ajout de la photo des techs dans la zone de défilement horizontale (BETA)

--- modules/affiche_techs.php
+++ modules/affiche_techs.php
@@ -4,5 +4,7 @@
 ?>
 
 <?php while($row = $stmt->fetch(PDO::FETCH_ASSOC)) : ?>
-    <div class="tech">Tech</div>
+    <div class="tech">
+        <img src="http://localhost/iCGS/ressources/images/Techs/<?php echo htmlspecialchars($row['identifiant']); ?>.png" width="256" height="256" alt="<?php echo htmlspecialchars($row['identifiant']); ?>" />
+    </div>
 <?php endwhile; ?>
