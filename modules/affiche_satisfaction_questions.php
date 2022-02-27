8872ae8bb35b0443f01aad79997f9b7f5fa552ca
Ryll974 <jullien.patrick@gmail.com>
2022-02-27 10:10:34 +0400

    ver.0.35 -- main/sondage - affichage des indices de satisfaction aux 4 questions (fonctionnel)

--- modules/affiche_satisfaction_questions.php
+++ modules/affiche_satisfaction_questions.php
@@ -1,4 +1,52 @@
-<img src="ressources/main/q1-satisfait.png" alt="q1">
-<img src="ressources/main/q2-satisfait.png" alt="q2">
-<img src="ressources/main/q3-satisfait.png" alt="q3">
-<img src="ressources/main/q4-satisfait.png" alt="q4">
+<?php
+
+  if ($satisfaction_mois_q1 >= 85) {
+    $label_q1 = "satisfait";
+  } elseif ($satisfaction_mois_q1 >= 50) {
+    $label_q1 = "neutre";
+  } else {
+    $label_q1 = "insatisfait";
+  }
+
+  if ($satisfaction_mois_q2 >= 85) {
+    $label_q2 = "satisfait";
+  } elseif ($satisfaction_mois_q2 >= 50) {
+    $label_q2 = "neutre";
+  } else {
+    $label_q2 = "insatisfait";
+  }
+
+  if ($satisfaction_mois_q3 >= 85) {
+    $label_q3 = "satisfait";
+  } elseif ($satisfaction_mois_q3 >= 50) {
+    $label_q3 = "neutre";
+  } else {
+    $label_q3 = "insatisfait";
+  }
+
+  if ($satisfaction_mois_q4 >= 85) {
+    $label_q4 = "satisfait";
+  } elseif ($satisfaction_mois_q4 >= 50) {
+    $label_q4 = "neutre";
+  } else {
+    $label_q4 = "insatisfait";
+  }
+
+?>
+
+<img src="ressources/main/q1-<?php echo $label_q1; ?>.png" alt="q1">
+</br>
+</br>
+</br>
+</br>
+<img src="ressources/main/q2-<?php echo $label_q2; ?>.png" alt="q2">
+</br>
+</br>
+</br>
+</br>
+<img src="ressources/main/q3-<?php echo $label_q3; ?>.png" alt="q3">
+</br>
+</br>
+</br>
+</br>
+<img src="ressources/main/q4-<?php echo $label_q4; ?>.png" alt="q4">
